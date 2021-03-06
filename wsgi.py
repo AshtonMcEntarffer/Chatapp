74b7752af86b274109378b952acf4022bd8356ca
Samuel Casey <76224164+Jeve-Stobs@users.noreply.github.com>
2021-03-05 20:19:58 -0500

    Config DJANGO_SETTINGS_MODULE

--- wsgi.py
+++ wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+sys.path.append('/root/Chatapp')
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
 
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
